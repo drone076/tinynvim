21df059f3af9abc4f6adee1fb90eff329e4c840e
Drone <drone076@yahoo.com>
2025-12-09 23:08:30 +0300

    add go to definition keymap

--- init.lua
+++ init.lua
@@ -224,6 +224,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
 vim.keymap.set('n', '<space>f', function()
   vim.lsp.buf.format { async = true }
 end, opts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 
 -- ============================================================================
